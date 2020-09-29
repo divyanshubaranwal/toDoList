20f0ebfddd0cc680a3f1694a393ce7f978a80918
Divyanshu Baranwal <divyanshu9210@gmail.com>
2020-09-29 17:05:12 +0530

    Add new ports, Procfile

--- app.js
+++ app.js
@@ -141,6 +141,11 @@ app.post("/delete", function(req, res) {
 //     res.redirect("/work");
 // })
 
-app.listen(3000, function() {
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function() {
     console.log("Server is hosted on port 3000.");
 })
